17a1d96b3b3d14967a9781a59c8d5ea549ac9a91
anik <anikm2283@gmail.com>
2022-06-25 22:45:26 +0600

    Added slider

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,7 +1,53 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import photo from "../../photo/5aba028cf8e6127988f1d28d863b0987.jpg";
+import "./Slider.styles.css";
 const Slider = () => {
   return (
-    <div>
-      <h1>slider</h1>
+    <div className="slider-wrapper">
+      <div className="container">
+        <Swiper
+          spaceBetween={10}
+          slidesPerView={5}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          navigation={true}
+        >
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={photo} alt="" className="slider-img" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
